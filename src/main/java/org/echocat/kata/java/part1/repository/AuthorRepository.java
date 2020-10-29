e60e4b87c52cb530301f51f4f2f93c6741795373
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2020-10-29 12:09:44 -0600

    add repositories

---
+++ src/main/java/org/echocat/kata/java/part1/repository/AuthorRepository.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.echocat.kata.java.part1.repository;
+
+import org.echocat.kata.java.part1.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author ignis
+ */
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, String> {
+    
+}
